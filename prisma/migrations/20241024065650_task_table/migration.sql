d67fe92dfa8871f2b1b45408d3a0a449cd3a35dc
Himanshu0422 <himanshumittal035@gmail.com>
2024-10-24 21:55:40 +0530

    prisma and required packages

---
+++ prisma/migrations/20241024065650_task_table/migration.sql
@@ -0,0 +1,16 @@
+-- CreateEnum
+CREATE TYPE "TaskStatus" AS ENUM ('NOT_ASSIGNED', 'PENDING', 'DONE');
+
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "dueDate" TIMESTAMP(3),
+    "status" "TaskStatus" NOT NULL DEFAULT 'PENDING',
+    "userId" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
